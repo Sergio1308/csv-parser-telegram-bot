67109a2c261a60022c8b8c44981a15e46b1c9b37
Sergei <sergi.haiduk@gmail.com>
2022-12-12 06:17:02 +0200

    implemented business logic

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import logging
 import csv
 import requests
 import json
+import db.database as db
 from aiogram import *
-from config import Config
+import config
 
 logging.basicConfig(level=logging.INFO)
-bot = Bot(token=Config.API_TOKEN)
+bot = Bot(token=config.API_TOKEN)
 dp = Dispatcher(bot)
 
 
@@ -25,10 +26,9 @@ async def echo(message: types.Message):
 
 @dp.message_handler(content_types=['document'])
 async def echo(message: types.Message):
-    if message.document.mime_type == Config.CSV_MIME_TYPE:
-        print(message.document.file_id, message.document.file_name)
-        get_file_api_url = f'https://api.telegram.org/bot{Config.API_TOKEN}/getFile'
-        get_file_content_api_url = f'https://api.telegram.org/file/bot{Config.API_TOKEN}/' + '{file_path}'
+    if message.document.mime_type == config.CSV_MIME_TYPE:
+        get_file_api_url = f'https://api.telegram.org/bot{config.API_TOKEN}/getFile'
+        get_file_content_api_url = f'https://api.telegram.org/file/bot{config.API_TOKEN}/' + '{file_path}'
         response = requests.post(url=get_file_api_url, params={'file_id': message['document']['file_id']})
         json_response = json.loads(response.content)
         if response.status_code != 200 or not json_response.get('ok'):
@@ -37,13 +37,20 @@ async def echo(message: types.Message):
         if response.status_code != 200:
             raise FileNotFoundError()
         reader = csv.reader(response.text.split('\n'), delimiter=';')
+        # filling lists
         csv_text = []
+        table_names = []
         for row in reader:
             csv_text.append(row)
-        print('table names:', csv_text[0])  # table names
-        for s in csv_text[1:-1]:
-            print('\t'.join(s))
-        await bot.get_file(message.document.file_id)
+        for name in csv_text[0]:
+            table_names.append(name)
+        # working with db
+        await message.answer('Adding parsed data from CSV file to database...')
+        db.create_tables(table_names)
+        row_data = csv_text[1:-1]
+        for fields in row_data:
+            db.add_new_columns(table_names, fields)
+        await message.answer(f'Success. {len(row_data)} rows added to database.')
     else:
         await message.answer('Expected .CSV file format, try again')
 
