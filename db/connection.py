e1be348196add76bee34e389eb1511ad9d746450
Sergei Haiduk <sergi.haiduk@gmail.com>
2022-12-12 08:30:02 +0200

    edited reference to variable

--- db/connection.py
+++ db/connection.py
@@ -3,7 +3,7 @@ import db.db_config as db
 from psycopg2.pool import SimpleConnectionPool
 from contextlib import contextmanager
 
-pool = SimpleConnectionPool(minconn=1, maxconn=10, dsn=db.DATABASE_URI)
+pool = SimpleConnectionPool(minconn=1, maxconn=10, dsn=db.DATABASE_URI_KEY)
 
 
 @contextmanager
